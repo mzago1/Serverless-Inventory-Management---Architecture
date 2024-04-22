86a2f028eee83be4d946417965de8504df3fe91e
mzago1 <mzago@tecracer.de>
2024-04-22 13:01:53 +0200

    correct files

--- serverless-inventory-management/table.tf
+++ serverless-inventory-management/table.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "inventory_files" {
-  name           = "inventory_files"
+  name           = "inventory_fils"
   billing_mode   = "PROVISIONED"
   read_capacity  = 5
   write_capacity = 5
@@ -8,5 +8,38 @@ resource "aws_dynamodb_table" "inventory_files" {
   attribute {
     name = "ItemId"
     type = "S"
+  }
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_dynamodb_table" "inventory_table" {
+  name           = "Inventory"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "ItemId"
+  
+  attribute {
+    name = "ItemId"
+    type = "S"
+  }
+  
+  attribute {
+    name = "Timestamp"
+    type = "S"
+  }
+  
+  attribute {
+    name = "WarehouseName"
+    type = "S"
+  }
+  
+  attribute {
+    name = "ItemName"
+    type = "S"
+  }
+  
+  attribute {
+    name = "StockLevelChange"
+    type = "N"
   }
 }
